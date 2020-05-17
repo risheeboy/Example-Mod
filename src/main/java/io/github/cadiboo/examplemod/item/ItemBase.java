37be6982686d48756b922b3b1a03cea4ab09d5d1
risheeboy <rishee.boy@gmail.com>
2020-05-17 17:33:37 +0800

    Tools (#2)
    * Added ruby axe, ruby pickaxe, ruby sword, ruby shovel, ruby hoe
    
    * Added ruby axe, ruby pickaxe, ruby sword, ruby shovel, ruby hoe

--- src/main/java/io/github/cadiboo/examplemod/item/ItemBase.java
+++ src/main/java/io/github/cadiboo/examplemod/item/ItemBase.java
@@ -4,6 +4,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
+
+    public ItemBase() {
+        super(new  Item.Properties().group(ItemGroup.FOOD));
     public ItemBase() {
         super(new  Properties().group(ItemGroup.FOOD));
     }
